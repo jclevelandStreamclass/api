a98c8ef3c727f308cf932219d705d1cc90e2711e
Miguel <miguelbermell@gmail.com>
2021-07-10 01:42:32 +0200

    User Service

--- services/userService.js
+++ services/userService.js
@@ -4,6 +4,25 @@ const HttpError = require('../utils/httpError');
 const { generateToken } = require('./jwtServices');
 const { insertUserSchema } = require('../validations/userValidation');
 
-exports.findAllUsers = async () => {
+exports.getAllUsers = async () => {
   return userRepository.findAllUsers();
 }
+
+exports.getUserById = async (id) => {
+  const user = await userRepository.findUserById();
+  return user.toJSON();
+}
+
+exports.getUserByEmail = async (email) => {
+  return userRepository.findUserByEmail(email)
+}
+
+exports.signUp = async (userData) => {
+  const validateUser = await insertUserSchema.validateAsync(userData);
+  const encryptedPassword = await encryptPassword(validateUser.password);
+
+  await userRepository.insertUser({
+    ...validateUser,
+    password: encryptedPassword
+  });
+};
