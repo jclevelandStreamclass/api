2c0b05a388c28147e270f8daaff04acd116469ed
Rubenrh <ruben.romeroherrero@gmail.com>
2021-07-09 19:21:08 +0200

    ruben branch

--- routes/series.js
+++ routes/series.js
@@ -1,56 +1,56 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const serieService = require("../services/serieService");
 
-// GET 
-router.get('/', async(req, res, next) => {
-    try {
-        const series = await serieService.getAllSeries();
-        res.status(200).json(series);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+// GET ALL
+router.get("/", async (req, res, next) => {
+  try {
+    const series = await serieService.getAllSeries();
+    res.status(200).json(series);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-router.get('/:id', async (req, res, next) => {
-    try {
-        const serie = await serieService.getAllSeries();
-        res.status(200).json(serie);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+router.get("/:id", async (req, res, next) => {
+  try {
+    const serie = await serieService.getAllSeries();
+    res.status(200).json(serie);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 // POST
-router.post('/', async (req, res, next) => {
-    try {
-        const serie = await serieService.createSerie(req.body);
-        res.status(200).json(serie);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+router.post("/", async (req, res, next) => {
+  try {
+    const serie = await serieService.createSerie(req.body);
+    res.status(200).json(serie);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 // PUT
-router.put('/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const serie = await serieService.editSerie(id);
-        res.status(200).json(serie);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const serie = await serieService.editSerie(id);
+    res.status(200).json(serie);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 // DELETE
-router.delete('/:id', async (req, res, next) => {
-     try {
-        const { id } = req.params;
-        const serie = await serieService.removeSerie(id);
-        res.sendStatus(204);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const serie = await serieService.removeSerie(id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
