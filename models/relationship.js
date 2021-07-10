c639c7a4641e8598192be6209586f154ef7c8a83
Jaime <jaime.mrnmrn@gmail.com>
2021-07-10 13:03:25 +0200

    Revisión de rutas sportsPlayer

--- models/relationship.js
+++ models/relationship.js
@@ -1,44 +1,29 @@
 const dbConnection = require("../config/db");
 
 const Serie = require("./Serie");
-const Episode = require('./Episode')
-const Category = require('./Category')
-const SportsPlayer = require("./SportsPlayer")
-const User = require('./User')
+const Episode = require("./Episode");
+const Category = require("./Category");
+const SportsPlayer = require("./SportsPlayer");
+const User = require("./User");
 
 const loadModels = () => {
+  User.hasMany(Serie);
 
-    User.hasMany(Serie, {
-        foreignKey: {allowNull: false},
-    });
+  Serie.belongsToMany(User, { through: "UserSeries" });
 
-    Serie.belongsToMany(User, {through: 'UserSeries'});
+  Serie.hasMany(Episode);
 
-    Serie.hasMany(Episode, {
-        foreignKey: {allowNull: false}
-    })
+  Episode.belongsTo(Serie);
 
-    Episode.belongsTo(Serie);
+  Category.hasMany(Serie);
 
-    Category.hasMany(Serie, {
-        foreignKey: { allowNull: false }
-    });
-    
-    Serie.belongsTo(Category);  
-         
-    SportsPlayer.hasMany(Serie, {
-       foreignKey: {allowNull: false}
-    });
-    
-    Serie.belongsTo(SportsPlayer);  
+  Serie.belongsTo(Category);
 
-    dbConnection.sync({force: true}).then(() => console.log("Estamos en el aire🤯🤯!!!!"));
+  SportsPlayer.hasMany(Serie);
 
+  Serie.belongsTo(SportsPlayer);
+
+  dbConnection.sync().then(() => console.log("Estamos en el aire🤯🤯!!!!"));
 };
 
 module.exports = loadModels;
-
-
-
-
-
--- services/userService.js
+++ services/userService.js
@@ -1,9 +1,9 @@
-const userRepository = require('../repositories/userRepository');
-const encryptPassword = require('../utils/encryptPassword');
-const HttpError = require('../utils/httpError');
-const { generateToken } = require('./jwtServices');
-const { insertUserSchema } = require('../validations/userValidation');
+const userRepository = require("../repositories/userRepository");
+const encryptPassword = require("../utils/encryptPassword");
+const HttpError = require("../utils/httpError");
+const { generateToken } = require("./jwtServices");
+const { insertUserSchema } = require("../validations/userValidation");
 
 exports.findAllUsers = async () => {
   return userRepository.findAllUsers();
-}
+};
