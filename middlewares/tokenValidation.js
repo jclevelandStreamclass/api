370a3fbbe7b4dcbca87da4c21bd3bed9f1559ae3
Miguel <miguelbermell@gmail.com>
2021-07-17 12:01:41 +0200

    changes

--- middlewares/tokenValidation.js
+++ middlewares/tokenValidation.js
@@ -3,8 +3,8 @@ const { validateToken } = require("../services/jwtServices");
 const tokenValidation = (req, res, next) => {
   if (req.headers.authorization) {
     const token = req.headers.authorization.slice(7);
-    const { id, name, email, role, phone } = validateToken(token);
-    req.user = { id, name, email, role, phone };
+    const { id, name, email, role, phone, active } = validateToken(token);
+    req.user = { id, name, email, role, phone, active };
   }
 
   next();
--- validations/serieValidation.js
+++ validations/serieValidation.js
@@ -4,6 +4,7 @@ exports.insertSerieSchema = Joi.object({
   title: Joi.string().min(1).required(),
   description: Joi.string().min(1).required(),
   photo: Joi.string().min(1).required(),
+  sportsPlayerId: Joi.string(),
 });
 
 exports.updateSerieSchema = Joi.object({
