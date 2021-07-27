10ade0476f01fc8bf9fc06b6d0543a6f2594a603
Madeleine Elliot <81318490+mad-prog@users.noreply.github.com>
2021-07-27 21:26:58 +0200

    limpiar comentarios

--- validations/episodeValidations.js
+++ validations/episodeValidations.js
@@ -4,7 +4,7 @@ exports.insertEpisodeSchema = Joi.object({
     title: Joi.string().max(100).required(),
     number: Joi.number().required(),
     duration: Joi.string().regex(/^([0-1]?\d|2[0-3])(?::([0-5]?\d))?(?::([0-5]?\d))?$/).required(),
-    //TODO validates time in string in HH:MM:SS format
+    // validates time in string in HH:MM:SS format
     description: Joi.string(),
     photo: Joi.string().required(),
     video: Joi.string().required(),
@@ -15,7 +15,7 @@ exports.updateEpisodeSchema = Joi.object({
     title: Joi.string().max(100),
     number: Joi.number(),
     duration: Joi.string().regex(/^([0-1]?\d|2[0-3])(?::([0-5]?\d))?(?::([0-5]?\d))?$/),
-    //TODO validates time in string in HH:MM:SS format
+    // validates time in string in HH:MM:SS format
     description: Joi.string(),
     photo: Joi.string(),
     video: Joi.string(),
